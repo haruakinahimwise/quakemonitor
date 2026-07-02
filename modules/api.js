40011a568428e902e76bc0a1846f4cbe0aecc188
haruakinahimwise <haruakinahimwise@gmail.com>
2026-07-02 11:49:16 +0900

    Update api.js

--- modules/api.js
+++ modules/api.js
@@ -1,1 +1,11 @@
-
+async function fetchLatestQuake() {
+  // TODO: replace with real JMA/NIED API later
+  // For now, return a fake quake near Tokyo
+  return {
+    lat: 35.68,
+    lon: 139.76,
+    shindo: 4,
+    regionName: "東京都",
+    originTime: new Date().toISOString()
+  };
+}
